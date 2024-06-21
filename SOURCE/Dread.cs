2ef927abada9efea53188a7010f00c0dc5f0bceb
sapph42 <31635854+sapph42@users.noreply.github.com>
2024-06-21 12:44:40 -0400

    Fun little feature

---
+++ SOURCE/Dread.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher {
+    public partial class Dread : Form {
+        public Dread() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Close();
+        }
+    }
+}
